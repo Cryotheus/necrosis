2efe6babb8d18a0f06fb813d8c479502805267a1
Cryotheus <wisedthecoder@gmail.com>
2023-03-15 17:22:52 -0600

    Remove redundant function declaration

--- gamemodes/necrosis/gamemode/global/hook.lua
+++ gamemodes/necrosis/gamemode/global/hook.lua
@@ -3,8 +3,5 @@ function GM:HookMethod(short_key)
 	if self[short_key] then return end
 	
 	local key = "Necrosis" .. short_key
-	
-	local function hook_run(_self, ...) return hook.Run(key, ...) end
-	
-	self[short_key] = hook_run
+	self[short_key] = function(_self, ...) return hook.Run(key, ...) end
 end
