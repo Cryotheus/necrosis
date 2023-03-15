1737c17e11cd825bc307632d8ed8fa00bfcd9238
Cryotheus <wisedthecoder@gmail.com>
2023-03-15 17:36:15 -0600

    Remove debug print

--- gamemodes/necrosis/gamemode/global/post.lua
+++ gamemodes/necrosis/gamemode/global/post.lua
@@ -1,9 +1,5 @@
 --post
 for key, value in pairs(GM) do
 	--call HookMethod for anything prexied by Necrosis
-	if isstring(key) and string.StartWith(key, "Necrosis") then
-		print("hi", key)
-		
-		GM:HookMethod(string.sub(key, 9))
-	end
+	if isstring(key) and string.StartWith(key, "Necrosis") then GM:HookMethod(string.sub(key, 9)) end
 end
