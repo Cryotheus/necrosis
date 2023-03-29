3ec9802bab22e1b6c502ca11d0afd32b75623afa
Cryotheus <wisedthecoder@gmail.com>
2023-03-29 17:43:10 -0600

    Fixed missed auto-hooks

--- gamemodes/necrosis/gamemode/global/post.lua
+++ gamemodes/necrosis/gamemode/global/post.lua
@@ -1,5 +1,11 @@
+--locals
+local to_hook = {}
+
 --post
 for key, value in pairs(GM) do
-	--call HookMethod for anything prexied by Necrosis
-	if isstring(key) and string.StartWith(key, "Necrosis") then GM:HookMethod(string.sub(key, 9)) end
+	--queue up a HookMethod call for anything prexied by Necrosis
+	--we have to queue, because modifying the table while iterating will skip indices with a big table
+	if isstring(key) and string.StartWith(key, "Necrosis") then table.insert(to_hook, string.sub(key, 9)) end
 end
+
+for index, short_key in ipairs(to_hook) do GM:HookMethod(short_key) end
